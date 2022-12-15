22e3609d85eb2e01cba3c0c7ec725a5100ce87fb
Khaetbek <hayotbek-web@mail.ru>
2022-12-15 19:36:29 +0500

    #fix #js Разработал показ всех постов

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const markdownResultElement = document.querySelector("#markdown-result");
+const allListElement = document.querySelector('#all-list');
 // Подключаем и парсим localStorage
 const articles = JSON.parse(localStorage.getItem('articles'));
 const article = articles[articles.length - 1];
@@ -8,3 +9,14 @@ const article = articles[articles.length - 1];
 //substr - для показа количества контента от 0 диопазона до n
 let result = marked.marked(article.content.substr(0, 250) + "...");
 markdownResultElement.innerHTML = result;
+
+// for
+let str = '';
+for (let i = 0; i < articles.length; i++) {
+    const currentIndexPost = articles[i];
+    str = str + `
+    <li class="other-list__item"><a class="other-list__link" href="#">${currentIndexPost.title}</a></li>
+    <li class="other-list__item
+    `;
+    allListElement.innerHTML = str;
+}
